92952d576671af30eff213ecdf5181ee0e13b04b
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-02-24 00:27:01 +0530

    Some test code removed from BaseHandler

--- app.blocks/handler/_type/handler_type_base.node.js
+++ app.blocks/handler/_type/handler_type_base.node.js
@@ -4,18 +4,6 @@ App.BaseHandler = inherit(App.CommonHandler, {
         this._params = App.Util.merge(this._getDefaultParams(), params);
 
         this._routes = this.__self._parse(this._params.routes);
-
-        // TODO: method, params
-        this
-            .addRoute({
-                rule   : '/objects/{id}',
-                action : 'objects',
-                method : 'get',
-                params : {
-                    'new' : true,
-                    'type' : ['one', 'two']
-                }
-            });
     },
 
     _getDefaultParams : function() {
