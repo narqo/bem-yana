422bf47c2aeb0678beab157eacfefb5302418f01
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-03-01 13:31:19 +0400

    http block refactoring

--- app.blocks/http/http.node.js
+++ app.blocks/http/http.node.js
@@ -1,18 +1,21 @@
 App.Http = inherit({
 
     __constructor : function(params) {
-        this._stack = [];
+        this._handlers = [];
 
         this._params = App.Util.merge(this._getDefaultParams() || params);
 
         this
-            ._loadMiddlewares()
+            ._loadHandlers()
             ._createServer();
     },
 
     run : function(port) {
-        App.Logger.debug('Server started on %d', port);
-        this._server.listen(port);
+        port || (port = App.Config.param('node').port);
+
+        this._server.listen(port, function() {
+            App.Logger.debug('Server started on %d', port);
+        });
     },
 
     stop : function() {
@@ -20,18 +23,20 @@ App.Http = inherit({
         this._server.close();
     },
 
-    _stack : [],
+    _handlers : [],
 
-    _loadMiddlewares : function() {
-        this._stack = this._params['handlers'];
+    _loadHandlers: function() {
+        this._handlers = this._params['handlers'];
         return this;
     },
 
     _onRequest : function(req, res) {
         App.Logger.debug('\nRequest for "%s" received', req.url);
 
-        var stack = this._stack,
-            resp = [];
+        var stack = this._handlers,
+            resP = [],
+            handler,
+            result;
 
         for(var i = 0; i < stack.length; i++) {
             if(res.finished) {
@@ -41,15 +46,18 @@ App.Http = inherit({
             }
 
             try {
-                var handler = (new stack[i]()).run();
-                resp.push(handler(req, res));
+                handler = (new stack[i]()).run();
+                result = handler(req, res);
+
+                Vow.isPromise(result) && resP.push(result);
             } catch(e) {
                 this._onError(req, res, e);
                 return;
             }
 
-            Vow.all(resp)
-                .then(this._onStackEnd.bind(this, req, res));
+            resP.length &&
+                Vow.all(resP)
+                    .then(this._onStackEnd.bind(this, req, res), this._onError.bind(this, req, res));
         }
     },
 
@@ -64,6 +72,9 @@ App.Http = inherit({
 
     _onStackEnd : function(req, res) {
         App.Logger.debug('All request handlers are passed');
+
+        // XXX: should we really need this?
+        res.finished || res.end();
     },
 
     _createServer : function() {
