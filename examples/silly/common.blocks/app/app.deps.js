ebeaf994b2adaff29796407ceba37ce10a442cde
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-04-14 17:31:30 +0400

    Example: global block renamed to app

---
+++ examples/silly/common.blocks/app/app.deps.js
@@ -0,0 +1,11 @@
+({
+    mustDeps: [
+        { block : 'modules' }
+    ],
+    shouldDeps : [
+        { block : 'http' },
+        { block : 'handler', mods : { 'type' : 'common' } },
+        { block : 'cluster' },
+        { elem : 'config' }
+    ]
+})
