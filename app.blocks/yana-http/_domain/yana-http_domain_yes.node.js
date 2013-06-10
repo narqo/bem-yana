6777bd82a4310af95bad01b576a2b3d8c818293b
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-06-10 04:32:19 +0400

    Domain error handeling refactoring (works proper under the Node.js<0.10)

--- app.blocks/yana-http/_domain/yana-http_domain_yes.node.js
+++ app.blocks/yana-http/_domain/yana-http_domain_yes.node.js
@@ -24,20 +24,30 @@ provide(inherit(Http, {
     },
 
     _onError : function(req, res, err) {
-        var base = this.__base,
-            reqd = req.domain;
+        var domain = req.domain,
+            onError = this.__base.bind(this);
+
+        // TODO: why do we need this stuff?
+        // @see https://github.com/isaacs/npm-www/blob/master/decorate.js
+        delete err.domain;
 
         // FIXME: it's seems like ugly hack!
         if(err instanceof YanaError) {
-            // Ok, this is our error, we know what to do
-            return base(req, res, err);
+            // Ok, this is yana-error, so we know what to do
+            onError(req, res, err);
+            return;
         }
 
-        // Anything can happen now! Be very careful!
-
         try {
-            logger.debug('Disposing domain stuff');
-            reqd.dispose();
+            res.on('finish', function() {
+                logger.debug('Response was finished, disposing domain stuff');
+                domain.dispose();
+            });
+
+            // Error handler should be postponed because of promises
+            process.nextTick(function() {
+                onError(req, res, err)
+            });
 
             if(CLUSTER.isWorker) {
                 // @see http://nodejs.org/api/all.html#all_warning_don_t_ignore_errors
@@ -51,11 +61,9 @@ provide(inherit(Http, {
                 // and then it will fork a new worker
                 CLUSTER.worker.disconnect();
             }
-
-            base(req, res, err);
         } catch(newerr) {
             logger.critical('Error sending proper error status for "%s"', req.url, newerr);
-            reqd.dispose();
+            domain.dispose();
         }
     }
 
