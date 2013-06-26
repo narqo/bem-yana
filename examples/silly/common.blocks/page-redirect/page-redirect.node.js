e9ddc7038e6de0e8ed37d81d017d4d5eaacc701a
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-06-26 23:42:41 +0400

    Examples tests logging level changed to "info"

--- examples/silly/common.blocks/page-redirect/page-redirect.node.js
+++ examples/silly/common.blocks/page-redirect/page-redirect.node.js
@@ -6,7 +6,7 @@ modules.define(
 View.decl({ name : 'page-redirect', base : 'page' }, {
 
     render : function() {
-        logger.debug('"%s" view should redirect to "/albums" with status code 302', this._getName());
+        logger.info('"%s" view should redirect to "/albums" with status code 302', this._getName());
         this.res.redirect('/albums');
     }
 
