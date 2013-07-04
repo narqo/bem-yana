66804fce1950db33d9cba9fc4f9790a469f7d7d8
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-07-04 23:07:13 +0300

    Minor bem-configs tuning

--- .bem/levels/blocks.js
+++ .bem/levels/blocks.js
@@ -1,11 +1,9 @@
 var PATH = require('path'),
-    environ = require('../environ'),
 
     join = PATH.join,
     resolve = PATH.resolve.bind(null, __dirname),
 
     PRJ_TECHS = resolve('../techs/');
-
 
 exports.getTechs = function() {
 
@@ -13,7 +11,6 @@ exports.getTechs = function() {
         'bemdecl.js'    : 'bemdecl.js',
         'deps.js'       : 'deps.js',
         'js'            : 'js-i',
-
         'node.js'       : join(PRJ_TECHS, 'node.js')
     };
 
