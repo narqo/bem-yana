9e6a06a73059a2ac340eca887dadd718f85f4126
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-07-22 16:50:06 +0400

    tests: test1 bundle's declaration updated

--- examples/tests/test.bundles/test1/test1.bemdecl.js
+++ examples/tests/test.bundles/test1/test1.bemdecl.js
@@ -1,9 +1,11 @@
 exports.blocks = [
     { block : 'app', mods : { start : 'auto' } },
     'page-home',
+    'page-internal-error',
     'page-404',
     'page-redirect',
     'page-methods',
     'page-error',
+    'page-reject',
     'page-leak'
 ];
