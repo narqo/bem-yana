36693828dba1a8c0fc3359802251e5228f62650c
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-09-21 02:54:41 +0400

    Custom BundleNode redefenition remove

--- .bem/nodes/bundle.js
+++ .bem/nodes/bundle.js
@@ -1,11 +1,11 @@
 module.exports = function(registry) {
 
-    registry.decl('BundleNode', {
-
-        'create-node.js-optimizer-node' : function(tech, sourceNode, bundleNode) {
-            return this['create-js-optimizer-node'].apply(this, arguments);
-        }
-
-    });
+//registry.decl('BundleNode', {
+//
+//    'create-node.js-optimizer-node' : function(tech, sourceNode, bundleNode) {
+//        return this['create-js-optimizer-node'].apply(this, arguments);
+//    }
+//
+//});
 
 };
