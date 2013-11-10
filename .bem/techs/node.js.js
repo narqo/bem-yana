9657c75e75aab1c87b58b3791c57535853195ba3
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-11-10 20:19:05 +0400

    Fix pathes generation for node.js tech on Windows

--- .bem/techs/node.js.js
+++ .bem/techs/node.js.js
@@ -1,3 +1,5 @@
+var PATH = require('path');
+
 exports.baseTechName = 'v2/js';
 exports.techMixin = {
 
@@ -9,7 +11,17 @@ exports.techMixin = {
         return ['node.js'];
     },
 
-    getBuildResultChunk : function(relPath, path, suffix) {
+    getBuildResultChunk : function(relPath) {
+        // NOTE: module's path is always an UNIX path,
+        // so `relPath` should be converted on Windows systems
+        if(PATH.sep !== '/') {
+            relPath = relPath.replace(/\\/g, '/');
+        }
+        // NOTE: Without a leading '/' or './' to indicate a file, the module is either
+        // a "core module" or is loaded from a node_modules folder
+        if(/^\w/.test(relPath)) {
+            relPath = './' + relPath;
+        }
         return 'require(\'' + relPath + '\');\n';
     }
 
